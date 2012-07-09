8e11caf6362211ed2d2e9adf7efba7a7732e2252
ymow <kenshin0812@hotmail.com>
2012-07-09 17:00:35 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redemo1::Application.config.secret_token = '5e50dd9cf211ce6fccb6b7b8ffae224c5cb1fb637f992735a4cab6293e03b871c57963b4470671a9f3b102534972b665149138813859fa30bf17387ca2ea64ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Redemo1::Application.config.session_store :cookie_store, key: '_Redemo1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Redemo1::Application.config.session_store :active_record_store
